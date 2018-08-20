569b9ef5a93bc44c27da837d6705a154d6f27820
Eric Developer <ea33sports@me.com>
2018-08-19 20:12:11 -0600

    View Hierarchy and Auto Layout commit

---
+++ View Hierarchy and Auto-Layout/View Hierarchy and Auto-Layout/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  View Hierarchy and Auto-Layout
+//
+//  Created by Eric Andersen on 8/19/18.
+//  Copyright © 2018 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let redView = UIView(frame: CGRect(x: 20, y: 20, width: 150, height: 150))
+//        redView.backgroundColor = .red
+//
+//        view.addSubview(redView)
+//
+//        let blueView = UIView(frame: CGRect(x: 20, y: 20, width: 100, height: 100))
+//        blueView.backgroundColor = .blue
+//
+//        redView.addSubview(blueView)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
